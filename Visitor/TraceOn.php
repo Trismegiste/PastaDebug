1a6f4e24ce9adb352eba4315b338c4db0131933b
Trismegiste <florent.g@allopneus.com>
2013-06-01 20:12:02 +0200

    cleaning

--- Visitor/TraceOn.php
+++ Visitor/TraceOn.php
@@ -20,19 +20,19 @@ class TraceOn extends \PHPParser_NodeVisitorAbstract
             case 'Expr_MethodCall':
                 if (!($node->var->getType() == 'Expr_Variable' &&
                         $node->var->name == 'this')) {
+
                     $newArg = array(
                         new \PHPParser_Node_Expr_ConstFetch(new \PHPParser_Node_Name('__METHOD__')),
                         $node->var,
                         new \PHPParser_Node_Scalar_String($node->name),
                         new \PHPParser_Node_Expr_Array($node->args)
                     );
+
                     return new \PHPParser_Node_Expr_StaticCall(
                             new \PHPParser_Node_Name_FullyQualified('Trismegiste\Intricate\PhpUnit\Command')
                             , 'methodCallCatcher'
                             , $newArg
                     );
-                    return new \PHPParser_Node_Expr_FuncCall(
-                            new \PHPParser_Node_Name('method_call_catcher'), $args);
                 }
                 break;
         }
